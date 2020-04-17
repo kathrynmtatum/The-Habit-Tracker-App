018dbd1b54d51c74f158ae37ea0c93902b8f72bb
Kathryn Tatum <tatumka@bc.edu>
2020-04-17 16:06:00 -0400

    Update 5

--- The Habit Tracker App/ItemDetailTableViewController.swift
+++ The Habit Tracker App/ItemDetailTableViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ItemDetailTableViewController: UITableViewController {
+    
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var noteView: UITextView!
+    
+    var habitItem: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        if habitItem == nil {
+            habitItem = ""
+        }
+        
+        nameField.text = habitItem
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        habitItem = nameField.text
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingFromProfile = presentingViewController is ProfileViewController
+        if isPresentingFromProfile {
+            dismiss(animated: true, completion: nil)
+        } else {
+            dismiss(animated: true, completion: nil)
+        }
+    }
 }
--- The Habit Tracker App/ProfileViewController.swift
+++ The Habit Tracker App/ProfileViewController.swift
@@ -12,11 +12,25 @@ class ProfileViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var habitArray = ["Wake up at 8am every day", "Go on a run every other day", "Eat 3 servings of vegetables every day", "Call parents once a week", "Call best friend once a week", "Volunteer once a week"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
 
+}
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return habitArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "\(habitArray[indexPath.row])"
+        return cell
+    }
 }
