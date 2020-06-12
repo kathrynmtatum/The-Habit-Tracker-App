b8675074ac79495d6c5f9da306ed08ff79691fa7
Kathryn Tatum <tatumka@bc.edu>
2020-06-12 10:57:22 -0400

    Final Push

--- The Habit Tracker App/BrowseDetailViewController.swift
+++ The Habit Tracker App/BrowseDetailViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class BrowseDetailViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    
     var selection: String!
     
     var habit: [String] = []
